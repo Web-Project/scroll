40cac2c14aa2f0b63d0e8fca71ae585a28d8662f
ebgutierrez <egeeboygutierrez91@gmail.com>
2013-06-19 04:32:42 -0300

    Update Config.php
    modified config

--- Config.php
+++ Config.php
@@ -4,7 +4,7 @@
 	$server = "localhost";
 	$db_user = "druidinc_scroll";
 	$db_pass = "scroll2013!!";
-	$link = mysql_connect($server, $db_user, $db_pass);
-	mysql_select_db($database, $link);
+	$link = mysql_connect($server, $db_user, $db_pass) or die('Error: cannot connect. ' . mysql_error());
+	mysql_select_db($database, $link)or die('Error: cannot select. ' . mysql_error());
 
 ?>
